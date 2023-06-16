5f0365c9ec51dcda0e4818a0c0df44a37a9d197c
Mohamedattia123 <underworld_100@yahoo.com>
2023-06-16 12:31:34 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - 101 quote.c
  *
- * Description: Write a C program that prints exactly without using printf or puts
+ * Description: Write a C program that prints exactly without using printf
  *
- * Return: Always 0 (success)
- */
+ * Return: Always 1 (fail)
+*/
 int main(void)
 {
 	write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		59);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	59);
 	return (1);
 }
