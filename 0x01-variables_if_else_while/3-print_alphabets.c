d19c4da9da134b1e4c90c4af3f48aba121729f2e
Mohamedattia123 <underworld_100@yahoo.com>
2023-06-19 07:59:50 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: using while to print a-z
+ *
+ * Return: Always 0 (success)
+*/
+        int main(void)
+{
+        char ch = 'a';
+
+        while (ch <= 'z')
+{
+        putchar(ch);
+        ch++;
+        }
+	while (ch1 <= 'Z')
+{
+	putchar(ch1);
+	ch1++;
+	}
+        putchar('\n');
+        return (0);
