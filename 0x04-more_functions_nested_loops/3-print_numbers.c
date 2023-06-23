45e577f03a049fcc0e62374281a3f32d022c782a
Mohamedattia123 <underworld_100@yahoo.com>
2023-06-23 02:18:48 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_numbers -  print number 0 - 0
+ *
+ * Description: using _putchar twice
+ *
+ * Return: return 0 (success)
+*/
+
+void print_numbers(void)
+
+{
+	char c = 0;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar('c');
+
+	}
+	_putchar('\n');
+	return (0);
+}
