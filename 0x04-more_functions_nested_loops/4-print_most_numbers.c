929cac39a35927803a794a226971329d98d3738e
Mohamedattia123 <underworld_100@yahoo.com>
2023-06-23 04:05:13 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,14 +11,13 @@
 void print_most_numbers(void)
 
 {
-	int c;
+	char c;
 
-	for (c = 0; c <= 9;)
+	for (c = 0; c <= 9; c++)
 	{
-	if (c == 2 || c == 4)
-		continue;
+	if (c != 2 || c != 4)
+
 	_putchar(c + 48);
-	c++;
 	}
 
 	_putchar('\n');
