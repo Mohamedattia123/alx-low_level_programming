ee377751fed14f0c93029c109d956a84d841c301
Mohamedattia123 <underworld_100@yahoo.com>
2023-06-23 02:47:56 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print char c
+ *
+ * using _putchar twice print from 0 > 9
+ *
+ * Return: return 0 (success)
+*/
+
+void print_most_numbers(void)
+
+{
+	char c = 0;
+
+	for (c = 0; c <= 9;)
+	{
+	if (c != 2 || c != 4)
+		{
+	_putchar(c + 48);
+
+		}
+	c++;
+	}
+
+	_putchar('\n');
+
+}
