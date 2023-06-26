2f2aded0cd31d817ace01e5ea3440df222b57134
Mohamedattia123 <underworld_100@yahoo.com>
2023-06-26 15:20:36 -0700

    2

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print string
+ *
+ * @str: string to print
+ *
+ *
+*/
+void _puts(char *str)
+{
+	while (*s != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
+
+
+}
