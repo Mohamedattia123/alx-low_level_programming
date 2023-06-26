6107f6de37bf7cf02687f6d800d7c9ae018f37ea
Mohamedattia123 <underworld_100@yahoo.com>
2023-06-26 13:27:31 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset value to 98
+ *
+ * @n: n updated to 98
+ *
+ *
+ *
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#def MAIN_H
+
+#include <stdio.h>
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
