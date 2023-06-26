fb5cf1582fa73d11926e05dca885f5240206ce65
Mohamedattia123 <underworld_100@yahoo.com>
2023-06-26 13:48:03 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaping between values
+ *
+ * @a: Parameter 1
+ * @b: Parameter 2
+ *
+*/
+void swap_int(int *a, int *b)
+{
+int tmp;
+	*a = tmp;
+	*b = *a;
+	*b = tmp;
+
+
+}
