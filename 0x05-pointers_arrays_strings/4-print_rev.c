3201d41f36130dd3fc461107fe8cbc09bbd716dd
Mohamedattia123 <underworld_100@yahoo.com>
2023-06-26 15:39:36 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,16 @@
 */
 void print_rev(char *s)
 {
-int longi = 0;
-int o;
+	int longi = 0;
+	int o;
+
 	while (*s != '\0')
 	{
 		longi++;
 		s++;
 	}
 	s--;
-	for (o = longi; 0 < 0; o--)
+	for (o = longi; o < 0; o--)
 	{
 		_putchar(*s);
 	s--;
