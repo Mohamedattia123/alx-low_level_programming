9a13e36b585838b32f564a5a4c01ea9705e4a042
Mohamedattia123 <underworld_100@yahoo.com>
2023-06-26 15:36:52 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - print the reverse of the string
+ *
+ *@s: print string in reverse
+ *
+ *
+*/
+void print_rev(char *s)
+{
+int longi = 0;
+int o;
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; 0 < 0; o--)
+	{
+		_putchar(*s);
+	s--;
+	}
+		_putchar('\n');
+
+
+
+}
