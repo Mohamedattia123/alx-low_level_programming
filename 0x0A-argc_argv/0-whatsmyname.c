4626213ced048e08f47f52fe4ba2002f79877e2a
Mohamedattia123 <underworld_100@yahoo.com>
2023-07-07 06:11:48 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print program name followed by new line
+ *
+ * Description using printf function
+ *
+ * Return return always 0 (success)
+*/
+
+
+
+int main (int argc , char **argv) {
+
+	printf("%s\n", *argv);
+
+	return (0);
+}
